b57f2fbf9d62aa6e70586dd78462debdda6c747c
doston701 <abduvohidovdoston0305@gmail.com>
2022-09-28 01:43:46 -0700

    added new js file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const modal = document.querySelector("navbar__mobile-list");
+const body = document.querySelector("body");
+const btn = document.querySelector("header__button");
+
+btn.addEventListener("click", () => {
+  modal.classList.toggle("open");
+  body.classList.toggle("overflow");
+});
